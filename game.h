9ac1283a96d89301e6f89024e2681d5999597943
Solène Fernandez Serrano <solene.fernandez-serrano@tuta.io>
2017-09-20 16:22:37 +0200

    Fix a human bug

--- game.h
+++ game.h
@@ -21,13 +21,18 @@ public:
     int findFreeSquareInColumn(int column);
     std::vector<std::vector<int>> changeStatusSquare(int column, int line, int player);
 
+    int getNbPlayers() const;
+    void setNbPlayers(int value);
+
     std::vector<char> getPlayerOutput() const;
     void setPlayerOutput(const std::vector<char> &value);
 
     void configMultiplayer(const int totalPlayer);
+
 private:
     int nbLines;
     int nbColumns;
+    int nbPlayers;
     std::vector<char> playerOutput = {' ', 'o', '#'};
 
     std::vector<std::vector<int>> gameboard;
